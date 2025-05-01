f3dedb37c780d84dbfbe72cc59a6cd46b1e17e5e
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-30 22:10:46 -0300

    rout com parametro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,14 @@ import { HomeComponent } from './home/home.component';
 import { ClienteComponent } from './components/cliente/cliente.component';
 import { ControlFlowComponent } from './components/control-flow/control-flow.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
+import { TesteComponent } from './components/teste/teste.component';
+import { ProdutoDetalheComponent } from './components/produto-detalhe/produto-detalhe.component';
 export const routes: Routes = [
 {path: '', component: HomeComponent},
 {path: 'cliente', component: ClienteComponent},
 {path: 'control-flow', component:ControlFlowComponent},
-{path: '**', component:NotfoundComponent}
+{path:'teste', component: TesteComponent},
+{path: 'produto/:id', component: ProdutoDetalheComponent},
+{path: '**', component:NotfoundComponent},
+
 ];
